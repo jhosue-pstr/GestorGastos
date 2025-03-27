1e5bf1ddf00d6167a5c3f2d7dcd11d5c0546345c
jhosue-pstr <pastor.jhosue07@gmail.com>
2025-03-26 19:58:38 -0500

    docker.yml

--- microservicio 1/app.py
+++ microservicio 1/app.py
@@ -1,129 +1,118 @@
-from flask import Flask, render_template, request, jsonify, make_response
+
+from flask import Flask, request, jsonify, make_response,render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 INFO = {
-    "lenguajes": ["Python", "Java", "JavaScript", "C#", "PHP"],
-    "frameworks": ["Flask", "Spring", "NodeJS", ".NET", "Laravel"],
-    "bases": ["React", "Angular", "Vue", "Django", "Ruby on Rails"]
+    "languages": {
+        "es":"Spanish",
+        "en":"English",
+        "fr":"French",
+    },
+    "colors":{
+        "r":"red",
+        "g":"green",
+        "b":"blue",
+    },
+    "clouds":{
+        "IBM":"IBM CLOUD",
+        "AMAZON":"AWS",
+        "MICROSOFT":"AZURE",
+    }
 }
 
-@app.route('/')
-def index():
-    return render_template('index.html', datos=INFO)
+@app.route("/")
+def home():
+   return "<h1 style='color:red'>This is home!</h1>"
 
+@app.route('/temp')
+def hello_world():
+    return render_template('index.html')
 
+@app.route("/qstr")
+def qs():
+    if request.args:
+        req = request.args
+        res = {}
+        for key, value in req.items():
+            res[key] = value
+        res = make_response(jsonify(res), 200)
+        return res
 
-# Obtener toda la información en formato JSON
-@app.route("/json", methods=['GET'])
-def obtener_json():
-    return make_response(jsonify(INFO), 200)
+    res = make_response(jsonify({"error": "No Query String"}), 404)
+    return res
 
+@app.route("/json")
+def get_json():
+    res = make_response(jsonify(INFO), 200)
+    return res
 
+@app.route("/json/<collection>/<member>")
+def get_data(collection, member):
+    print("getting the value of %s in the collection %s"%(member,collection))
+    if collection in INFO:
+        member = INFO[collection].get(member)
+        if member:
+            res = make_response(jsonify({"res":member}), 200)
+            return res
 
-# Obtener todas las categorías disponibles
-@app.route("/categorias", methods=['GET'])
-def obtener_categorias():
-    return jsonify({"categorias": list(INFO.keys())})
+        res = make_response(jsonify({"error": "Not found"}), 404)
+        return res
 
+    res = make_response(jsonify({"error": "Not found"}), 404)
+    return res
 
+# Post Method
 
-# Obtener una categoría específica
-@app.route("/categoria/<string:categoria>", methods=['GET'])
-def obtener_categoria(categoria):
-    if categoria in INFO:
-        return jsonify({categoria: INFO[categoria]})
-    return jsonify({"error": "Categoría no encontrada"}), 404
+@app.route("/json/<collection>", methods=["POST"])
+def create_col(collection):
 
+    req = request.get_json()
 
+    if collection in INFO:
+        res = make_response(jsonify({"error": "Collection already exists"}), 400)
+        return res
 
-# Agregar un nuevo elemento a una categoría
-@app.route("/categoria/<string:categoria>/agregar", methods=['POST'])
-def agregar_elemento(categoria):
-    if categoria in INFO:
-        data = request.get_json()
-        nuevo_elemento = data.get("elemento")
-        if nuevo_elemento:
-            INFO[categoria].append(nuevo_elemento)
-            return jsonify({"mensaje": f"{nuevo_elemento} agregado a {categoria}", categoria: INFO[categoria]})
-        return jsonify({"error": "Falta el campo 'elemento' en el JSON"}), 400
-    return jsonify({"error": "Categoría no encontrada"}), 404
+    INFO.update({collection: req})
 
+    res = make_response(jsonify({"message": "Collection created"}), 201)
+    return res
 
+# Put Method
 
-# Actualizar un elemento en una categoría
-@app.route("/categoria/<string:categoria>/actualizar", methods=['PUT'])
-def actualizar_elemento(categoria):
-    if categoria in INFO:
-        data = request.get_json()
-        elemento_antiguo = data.get("elemento_antiguo")
-        elemento_nuevo = data.get("elemento_nuevo")
-        
-        if elemento_antiguo in INFO[categoria]:
-            index = INFO[categoria].index(elemento_antiguo)
-            INFO[categoria][index] = elemento_nuevo
-            return jsonify({"mensaje": f"{elemento_antiguo} actualizado a {elemento_nuevo} en {categoria}", categoria: INFO[categoria]})
-        
-        return jsonify({"error": f"'{elemento_antiguo}' no encontrado en {categoria}"}), 404
-    return jsonify({"error": "Categoría no encontrada"}), 404
+@app.route("/json/<collection>/<member>", methods=["PUT"])
+def put_col_mem(collection,member):
 
+    req = request.get_json()
 
+    if collection in INFO:
+        if member:
+            print(req)
+            INFO[collection][member] = req["new"]
+            res = make_response(jsonify({"res":INFO[collection]}), 200)
+            return res
 
-# Eliminar un elemento de una categoría
-@app.route("/categoria/<string:categoria>/eliminar", methods=['DELETE'])
-def eliminar_elemento(categoria):
-    if categoria in INFO:
-        data = request.get_json()
-        elemento = data.get("elemento")
-        if elemento in INFO[categoria]:
-            INFO[categoria].remove(elemento)
-            return jsonify({"mensaje": f"{elemento} eliminado de {categoria}", categoria: INFO[categoria]})
-        return jsonify({"error": f"'{elemento}' no encontrado en {categoria}"}), 404
-    return jsonify({"error": "Categoría no encontrada"}), 404
+        res = make_response(jsonify({"error": "Not found"}), 404)
+        return res
 
+    res = make_response(jsonify({"error": "Not found"}), 404)
+    return res
 
+# Delete Method
 
-# Eliminar una categoría completa
-@app.route("/categoria/<string:categoria>", methods=['DELETE'])
-def eliminar_categoria(categoria):
-    if categoria in INFO:
-        del INFO[categoria]
-        return jsonify({"mensaje": f"Categoría {categoria} eliminada"})
-    return jsonify({"error": "Categoría no encontrada"}), 404
+@app.route("/json/<collection>", methods=["DELETE"])
+def delete_col(collection):
 
+    if collection in INFO:
+        del INFO[collection]
+        res = make_response(jsonify(INFO), 200)
+        return res
 
+    res = make_response(jsonify({"error": "Collection not found"}), 404)
+    return res
 
-
-# Crear una nueva categoría con elementos iniciales
-@app.route("/categoria/agregar", methods=['POST'])
-def agregar_categoria():
-    data = request.get_json()
-    nueva_categoria = data.get("categoria")
-    elementos = data.get("elementos", [])
-
-    if nueva_categoria in INFO:
-        return jsonify({"error": "La categoría ya existe"}), 400
-    
-    INFO[nueva_categoria] = elementos
-    return jsonify({"mensaje": f"Categoría {nueva_categoria} agregada", "INFO": INFO})
-
-
-
-# Buscar un elemento en todas las categorías
-@app.route("/buscar", methods=['GET'])
-def buscar_elemento():
-    query = request.args.get("query", "").lower()
-    resultados = {}
-
-    for categoria, elementos in INFO.items():
-        coincidencias = [item for item in elementos if query in item.lower()]
-        if coincidencias:
-            resultados[categoria] = coincidencias
-
-    if resultados:
-        return jsonify(resultados)
-    
-    return jsonify({"mensaje": "No se encontraron coincidencias"}), 404
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    PORT = 5000  # Definir el puerto antes de usarlo
+    print("Server running in port %s" % PORT)
+    app.run(host='0.0.0.0', port=PORT)
